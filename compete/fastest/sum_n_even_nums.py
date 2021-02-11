de0945612e5fa3fb2a836730d93b32c4c767e69e
AYehia0 <ahmedyehia611@gmail.com>
2021-02-11 23:14:41 +0200

    knowing the right equation is helpful

---
+++ compete/fastest/sum_n_even_nums.py
@@ -0,0 +1,38 @@
+"""
+PROBLEM:
+You must ouput the sum of the first N even natural numbers.
+For example, if N=4 we get 2, 4, 6, 8 and the sum of these numbers is 20.
+
+Line 1: An integer N for the number of even natural numbers to sum.
+
+Line 1 : The sum.
+CONSTRAINS:
+0 ≤ N ≤ 10^9
+EXAMPLE input:
+4
+EXAMPLE output:
+20
+------------
+4
+20
+5
+30
+2
+6
+1
+2
+10
+110
+3
+12
+2000
+4002000
+1000000000
+1000000001000000000
+
+"""
+
+# prove it using A.P.(Arithmetic Progression)
+
+print(n*(n+1))
+
