1b4b38c7b1bfb1554c6384fcc1a81db8f1fa3728
AYehia0 <ahmedyehia611@gmail.com>
2021-02-11 23:52:12 +0200

    count vowels, hehe regex boi

--- compete/shortest/count_vowels_again.py
+++ compete/shortest/count_vowels_again.py
@@ -24,5 +24,12 @@ I'm sure you will figure it out.
 12
 
 """
+# Can you solve using regex ?
+# Actually test for is stupid and could be cheated by adding y to the letters, instead of using lower lol
 
 print(sum(map(input().count,"aeiouy")))
+
+# my sol 
+
+print(len([i for i in input()if i in'aeiouy']))
+
